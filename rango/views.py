14dfa065f3ec28ac06d1ee23d00c1d81d0145868
Dominykas Meistas <2404288m@student.gla.ac.uk>
2020-02-12 19:01:06 +0200

    Chapter 10 + exercises completed, all tests pass

--- rango/views.py
+++ rango/views.py
@@ -8,9 +8,38 @@ from django.urls import reverse
 from rango.forms import PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 #def index(request):
 #	return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+	
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val	
+	
+#Updated the function definition
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+											   'last_visit',
+											   str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+										'%Y-%m-%d %H:%M:%S')
+										
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+		
+	# Update/set the visits cookie
+	request.session['visits'] = visits
 	
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
@@ -26,12 +55,22 @@ def index(request):
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
 	
-	# Render the response and send it back!
-	return render(request, 'rango/index.html', context=context_dict)
+	visitor_cookie_handler(request)
+	
+	# Obtain our Response object early so we can add cookie information.
+	response = render(request, 'rango/index.html', context=context_dict)
+	
+	# Return response back to the user, updating any cookies that need changed.
+	return response
 	
 def about(request):
+	
+	context_dict = {}
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+	
 	#return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-	return render(request, 'rango/about.html')
+	return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
@@ -234,5 +273,6 @@ def user_logout(request):
 def restricted(request):
 	# return HttpResponse("Since you're logged in, you can see this text!")
 	return render(request, 'rango/restricted.html')
+
 		
 				
